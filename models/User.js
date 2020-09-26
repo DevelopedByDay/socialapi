bdbff15ec73ecf1a5f10740c02bc2a2ce12faa34
BradD <bradleyeday@gmail.com>
2020-09-26 15:11:45 -0500

    models

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const moment = require('moment');
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: "You must enter a unique username!",
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: "You must enter a unique email address!",
+        match: `/.+\@.+\..+/`
+    },
+    throughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
+
