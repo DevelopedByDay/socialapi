ca32cfc511e0ca5b076f3a49abc51a34eac563fa
BradD <bradleyeday@gmail.com>
2020-10-11 11:25:46 -0500

    adjusted routes and added video links in readMe

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ This is a basic api that would be used by a social network application. It uses 
 
 ## Video Walkthroughs
 
-[!Users]("")
-[!Thoughts]("")
+[!Users]("https://drive.google.com/file/d/1IgZjnUAmV5DCyBc-yTDzAoIIcANqn-hH/view")
+[!Thoughts]("https://drive.google.com/file/d/11fJNHbSZZWDmowUPL61jInfd7HQKDAp8/view")
+[!Reactions]("https://drive.google.com/file/d/1FBR0y7khYfceZA2-P3MwGdRcCMsYwd32/view")
--- models/Thought.js
+++ models/Thought.js
@@ -2,33 +2,7 @@ const moment = require('moment');
 const { Schema, model, Types } = require('mongoose');
 const reactionSchema = require('./Reaction');
 
-const ReactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => Types.ObjectId()
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 280
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-        }
-    },
-    {
-        toJSON: {
-            getters: true
-        }
-    }
-)
+
 
 const ThoughtSchema = new Schema ({
     thoughtText: {
